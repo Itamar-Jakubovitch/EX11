0d2a7473e3b302d6fe2970c89b92ffd795f8639a
itamar_jakubovitch <Itamarjakubovitch@gmail.com>
2026-03-15 11:04:47 +0200

    completing the task, before the javadoc output

--- app/src/main/java/com/example/ex11/MainActivity.java
+++ app/src/main/java/com/example/ex11/MainActivity.java
@@ -1,24 +1,75 @@
 package com.example.ex11;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+/**
+ * @author Itamar Jakubovitch ItamarJakubovitch@gmail.com
+ * @version 1.0
+ * @since 14/3/2026
+ * A browser app
+ */
 public class MainActivity extends AppCompatActivity {
+    WebView wV;
+    Button btn;
+    EditText eT;
+    String stringUrl;
 
+    /**
+     * Runs when the app starts.
+     * <p>
+     *
+     * @param savedInstanceState saved app data from before
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        wV = (WebView) findViewById(R.id.wV);
+        eT = (EditText) findViewById(R.id.eT);
+        btn = (Button) findViewById(R.id.btn);
+
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.setWebViewClient(new MyWebViewClient());
+    }
+
+    /**
+     * Opens the website the user typed.
+     * <p>
+     *
+     * @param view the go button
+     */
+    public void clicked(View view) {
+        stringUrl = eT.getText().toString();
+        wV.loadUrl(stringUrl);
+    }
+
+    /**
+     * Makes links open inside the app and not in Chrome.
+     * <p>
+     *
+     */
+    private class MyWebViewClient extends WebViewClient {
+
+        /**
+         * Opens the link inside the app.
+         * <p>
+         *
+         * @param view the web browser inside the app
+         * @param url the link to open
+         * @return true so the app handles the link
+         */
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
